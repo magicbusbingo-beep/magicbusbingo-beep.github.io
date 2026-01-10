526c615aff2fdb8f31a55fc69303dd8be838d16d
magicbusbingo-beep <magicbusbingo@gmail.com>
2026-01-09 19:38:02 -0700

    Add files via upload

---
+++ git site/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+
+//socket.io setup
+const http = require('http');
+const server = http.createServer(app);
+const port = 3000
+
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+app.use(express.static('public'))
+
+app.get('/', (req, res)=>{
+	res.sendFile(__dirname + "index.html")
+})
+
+io.on('connection', (socket) =>{
+	console.log('a user connected! :>');
+});
+
+server.listen(port, ()=> {
+	console.log('Example app listening on port ${port}')
+})
