66210d5ab7ef91b2162742d6b7926f226d820b71
magicbusbingo-beep <magicbusbingo@gmail.com>
2026-05-23 07:58:10 -0700

    Add files via upload

---
+++ src/scenes/game.js
@@ -0,0 +1,19 @@
+// "Every great game begins with a single scene. Let's make this one unforgettable!"
+export class Game extends Phaser.Scene {
+    constructor() {
+        super('Game');
+    }
+
+    init() {
+        // Initialize scene
+    }
+
+    preload() {
+        // Load assets
+    }
+
+    create() {
+        // Create game objects
+    }
+
+}
